6ae1f7d34ce3f9bc3872567ce8f5abae7f4cba37
L200180150 <44252066+L200180150@users.noreply.github.com>
2018-10-18 16:55:53 +0700

    shell kegiatan 1 dan 2
    hasil dari shell kegiatan 1 dan 2

---
+++ tugas praktikum yudha gana p.w L200180150/shell kegiatan 1.py
@@ -0,0 +1,31 @@
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: D:\tugas praktikum yudha gana p.w L200180150\Kegiatan 1 dan 2.py =
+>>> Nama
+'Yudha Gana Prasetyo Wibowo'
+>>> Nim
+'L200180150'
+>>> Status
+'Mahasiswa'
+>>> Asal
+'Sma Negeri 1 Tawangasari'
+>>> Sekolah
+'Universitas Muhammadiyah Surakarta'
+>>> Fakultas
+'Fakultas Komunikasi Dan Informatika'
+>>> Prodi
+'Informatika'
+>>> Kelas
+'C dan D'
+>>> TTL
+'Klaten, 24 Juli 2001'
+>>> 
+>>> #kegiatan 2
+>>> Nama
+'Yudha Gana Prasetyo Wibowo'
+>>> Username
+'Y242001'
+>>> Password
+'Yud756'
+>>> 
